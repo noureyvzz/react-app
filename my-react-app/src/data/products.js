00796ffc28afe96bf9e2f963f9f631e7d3b09522
codexi <144174028+noureyvzz@users.noreply.github.com>
2024-11-25 13:43:39 -0800

    first-commit

---
+++ my-react-app/src/data/products.js
@@ -0,0 +1,11 @@
+import Product from "../classes/product";
+
+const products = [
+    new Product(1, "Laptop", 2000, 1000, 10),
+    new Product(2, "Frdge", 1000, 600, 5),
+    new Product(3, "phones", 1050, 120),
+    new Product(4, "Grinder", 150, 50, 40),
+    new Product(5, "TV", 300, 200, 15),
+];
+
+export default products;
